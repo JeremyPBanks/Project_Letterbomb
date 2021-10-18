34479ba8d02ce41f803124fa4d7a63737ae86fe4
Jeremy Banks <jaypb08@gmail.com>
2021-10-17 22:23:26 -0400

    10-17-2021 - Completed logic for sum of letter objects currently on board

--- Assets/Scripts/BoardPlacement.cs
+++ Assets/Scripts/BoardPlacement.cs
@@ -48,6 +48,10 @@ public class BoardPlacement : MonoBehaviour
     //~~JEREMY: Adds letters to custom dictionary. Indexes of letters is determined by class of frequency (Key=index of letter,Value=sprite)
     public void AddLetterSum() {
         int sum = 0;
-        Debug.Log("Buttton-->" + String.Join(", ", getKeysToActiveLetters()));
+        List<string> keysToActiveLetters = getKeysToActiveLetters();
+        foreach (String letterObjectName in keysToActiveLetters) {
+            sum += letters_used_this_turn[letterObjectName].GetComponent<DragObject>().value;
+        }
+        Debug.Log("Buttton-->" + sum);//String.Join(", ", getKeysToActiveLetters()));
     }
 }
